6000ac958898b26e4890a1bb636fa0501f315985
quietgong <quietgong@naver.com>
2024-09-20 15:05:02 +0900

    test

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("../public/service-worker.js")
+      .register("service-worker.js")
       .then((registration) => {
         console.log(
           "Service Worker registered with scope:",
